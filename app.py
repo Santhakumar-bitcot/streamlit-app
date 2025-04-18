83a1734fb81d533f56b17de06a71ba64c3d5ea1c
santhosh <santhosh.annamalai@resmed.co.in>
2025-04-18 12:08:45 +0530

    Happy modified in app.py file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("📊 Streamlit App - SANDY")
+st.title("📊 Streamlit App - HAPPY HAPPY HAPPY")
 
 st.sidebar.header("User Input")
 num_points = st.sidebar.slider("Number of data points", 10, 100, 50)
